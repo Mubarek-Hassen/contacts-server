72a22d5d55b765c83d6829a78ba846319f523b7a
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-05-10 09:13:32 -0400

    test route success

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import express from "express"
 import cors from "cors"
-import mongoose from "mongoose"
 import dotenv from "dotenv"
-
+import "./src/config/db.connection.js"
 dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//MIDDLEWARES
+app.use(cors())
+app.use(express.json())
+
+app.get("/test", (req, res)=> res.send("Test route!"))
 
 app.listen(PORT, ()=> console.log(`Server running on PORT: ${PORT}`))
