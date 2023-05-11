db295afc6fb3113d782c8977fe591c9905afe763
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-05-11 13:33:52 -0400

    user model added

---
+++ src/models/user-model.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: [true, "Please add a name."]},
+  email: {type: String, required: [true, "Please add an email."], unique: true},
+  password: {type: String, required: [true, "please add a password."]},
+},
+{
+  timestamps: true
+})
+
+const userModel = mongoose.model("User", userSchema)
+
+export default userModel
