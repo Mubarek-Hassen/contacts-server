dceb87b474d2adfd1e30a55aadf57c5776bb9747
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-05-13 22:03:41 -0400

    salt errors

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import router from "./src/routes/user-routes.js"
 import "./src/config/db.connection.js"
+
 dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,7 +11,9 @@ const PORT = process.env.PORT || 3001;
 //MIDDLEWARES
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use("/user", router)
 
-app.get("/test", (req, res)=> res.send("Test route!"))
+// app.get("/test", (req, res)=> res.send("Test route!"))
 
 app.listen(PORT, ()=> console.log(`Server running on PORT: ${PORT}`))
--- src/controllers/user-controllers.js
+++ src/controllers/user-controllers.js
@@ -1,7 +1,8 @@
-import userModel from "../models/user-model";
+import userModel from "../models/user-model.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
+// REGISTER USER
 export const registerUser = async (req,res)=>{
   const { name, email, password } = req.body;
   const user = await userModel.findOne({ email });
@@ -9,8 +10,9 @@ export const registerUser = async (req,res)=>{
     return res.json({msg: "User already exist!"})
   }
   const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
+  const hashedPassword = await bcrypt.hash(salt, password);
   const newUser = await userModel.create({ email, name, password: hashedPassword })
   res.json({ msg: "user registered successfully!"})
 }
 
+//LOGIN USER
