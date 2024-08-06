c095a585bd890e36a2b3eb4793830f1b43a8a117
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2024-08-06 14:23:18 -0400

    server crash when empty register input avoided

--- src/controllers/user-controllers.js
+++ src/controllers/user-controllers.js
@@ -5,11 +5,15 @@ import bcrypt from "bcrypt";
 // REGISTER USER
 const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
+  let errors = {}
+  if(!name) errors.name = "Name field cannot be empty!"
+  if(!email) errors.email = "Email field cannot be empty!"
+  if(!password) errors.password = "Password field cannot be empty!"
 
-  if(!name || !email || !password){
-    return res.json({msg: "Some input is empty"})
-  }
-try {
+  try {
+    if(!name || !email || !password){
+      return res.json(errors)
+    }
   const user = await userModel.findOne({ email });
   if (user) {
     return res.json({ msg: "User already exist!" });
@@ -35,19 +39,23 @@ try {
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
+  try {
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(401).send({msg: "email does not exist in database"})
+    }
+    
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({msg: "invalid - password"})
+    }
+    
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+    return res.json({ token, userId: user._id });
+  } catch (error) {
+    console.error("This is a login error===> ", error)
+  }
 
-  const user = await userModel.findOne({ email });
-  if (!user) {
-    return res.status(401).send({msg: "email does not exist in database"})
-  }
-  
-  const isPasswordValid = await bcrypt.compare(password, user.password);
-  if (!isPasswordValid) {
-    return res.status(401).json({msg: "invalid - password"})
-  }
-  
-  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
-  return res.json({ token, userId: user._id });
 };
 
 export { loginUser, registerUser };
